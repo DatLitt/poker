1937c7a86accb2dd57c24404595561018ac36287
DatLitt <141332767+DatLitt@users.noreply.github.com>
2026-03-11 15:08:23 +0700

    add all in button

--- frontend/src/components/ActionButtons/ActionButtons.tsx
+++ frontend/src/components/ActionButtons/ActionButtons.tsx
@@ -7,6 +7,7 @@ function ActionButtons({ actionAllowed }: { actionAllowed: string[] }) {
       <button disabled={!actionAllowed?.includes("check")}>Check</button>
       <button disabled={!actionAllowed?.includes("call")}>Call</button>
       <button disabled={!actionAllowed?.includes("raise")}>Raise</button>
+      <button disabled={!actionAllowed?.includes("all_in")}>All In</button>
     </div>
   );
 }
